f017d060c0d7f53395be7a1a078e2fd6df0fd845
Sandace <gsandace@gmail.com>
2022-04-25 18:53:30 +0545

    alarm page screen

---
+++ lib/pages/alarm_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:clock_app/pages/add_alarm_page.dart';
+class AlarmPage extends StatefulWidget {
+  const AlarmPage({Key? key}) : super(key: key);
+
+  @override
+  State<AlarmPage> createState() => _AlarmPageState();
+}
+
+class _AlarmPageState extends State<AlarmPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(child: Text('Press the button below!')),
+      floatingActionButton: FloatingActionButton.extended(
+        
+        onPressed: () {
+           Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddAlarms()),
+          );
+        },
+        
+        backgroundColor: Colors.grey,
+        label: const Text("Add Alarm", style: TextStyle(color: Colors.black),),
+        icon:const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
